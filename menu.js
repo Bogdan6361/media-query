2cbf19cd241e77f2127d54302236e12432b9379b
Bogdan6361 <74094803+Bogdan6361@users.noreply.github.com>
2021-03-04 20:18:12 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,23 @@
+const landscapeMQ = window.matchMedia("(orientation: landscape)");
+
+const noticeMessage = document.getElementById("notice");
+const start = document.getElementById("start");
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    noticeMessage.style.display = "none";
+    start.style.display = "block";
+    console.log(e.matches);
+  } else {
+    noticeMessage.style.display = "block";
+    start.style.display = "none";
+    console.log(e.matches);
+  }
+}
+
+// Register event listener
+landscapeMQ.addListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(landscapeMQ);
